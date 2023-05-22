6018d12d161927e16e1fd776c263160f446ba932
Pap-Ski <niilaryeaquarteypapafio@gmail.com>
2023-05-22 12:59:27 +0000

    Refactored rate-selection

--- script.js
+++ script.js
@@ -5,22 +5,13 @@ const selection = document.querySelector("span");
 const rateSelectionContainer = document.querySelector(".rate-selection");
 const rateRemarkContainer = document.querySelector(".rate-remark");
 
-let selectedRate;
-//   console.log("hello");
-
 ratings.forEach((rate) => {
   rate.addEventListener("click", (e) => {
-    rate.classList.add("selected");
-    selectedRate = e.currentTarget;
-    ratings.forEach((rate) => {
-      if (rate !== e.currentTarget) rate.classList.remove("selected");
-    });
+    selection.textContent = e.currentTarget.innerText;
   });
 });
 
 submitBtn.addEventListener("click", () => {
-  console.log(selectedRate);
-  selection.textContent = selectedRate.textContent;
   rateSelectionContainer.setAttribute("data-hidden", true);
   rateRemarkContainer.setAttribute("data-hidden", false);
 });
